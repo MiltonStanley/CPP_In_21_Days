f1e54a185d5d519fc73518203b3a778684c75c50
milton.stanley@gmail.com <milton.stanley@gmail.com>
2012-10-04 20:29:58 -0500

    Finished Day 2

---
+++ Day2/2_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello there.\n";
+  std::cout << "Here is 5: " << 5 << "\n";
+  std::cout << "The manipulator endl writes a new line to the screen." << std::endl;
+  std::cout << "Here is a very big number:\t" << 70000 << std::endl;
+  std::cout << "Here is the sum of 8 and 5:\t" << 8+5 << std::endl;
+  std::cout << "Here's a fraction:\t\t" << (float) 5/8 << std::endl;
+  std::cout << "And here's a very very big number:\t" << (double) 7000 * 7000 << std::endl;
+  std::cout << "Don't forget to replace Jesse Liberty with your name...\n";
+  std::cout << "Milton Stanley is a C++ programmer!\n";
+    return 0;
+}
---
+++ Day2/2_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+  /* this is a comment
+  and it extends until the closing
+  star-slash comment mark */
+  std::cout << "Hello World\n";
+  // this comment ends at the end of the line
+  std::cout << "That comment ended!\n";
+  
+  // double slash comments can be alone on a line
+  /* as can slash-star comments */
+    return 0;
+}
+  
