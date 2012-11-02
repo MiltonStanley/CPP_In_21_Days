8d01f9b159722e533996efe1b5bb9c4f76bb5e03
acer <acer@acer.(none)>
2012-11-02 14:28:44 -0500

    working on var-length arrays

---
+++ Sandbox/Array/varlength.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	using namespace std;
+	string one[3] = { "one", "two", "three"};
+	cout << one[2] << endl;
+	return 0;
+}
+
+//http://stackoverflow.com/questions/1454843/variable-length-array-of-classes
+//https://groups.google.com/forum/?fromgroups=#!topic/gnu.g++.help/pUJAALS2gyU
